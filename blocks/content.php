34c32a7a8c76883ee8d8f5003ff5daea5f3d4575
lecbit <artm-marych@rambler.ru>
2021-02-14 16:57:15 +0100

    pagination

--- blocks/content.php
+++ blocks/content.php
@@ -1,16 +1,27 @@
 <div class="container">
 <?php
 $num = 0;
+
+$page = isset($_GET['page']) ? $_GET['page']: 1;
+$limit = 6;
+$offset = $limit * ($page - 1);
+$post = get_posts($limit,$offset);
+
 for ($i=0; $i < 8/3; $i++) { ?>
   <div class="card-deck mb-3 text-center">
-  <?php for ($j=0; $j < 3; $j++) { ?>
+  <?php
+  for ($j=0; $j < 3; $j++) { 
+    if(!isset($post[$num])){
+      break;
+    }
+    ?>
     <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal"><?php $news = get_post_by_id($num+1);echo $news["title"];?></h4>
+            <h4 class="my-0 font-weight-normal"><?php echo $post[$num]["title"];?></h4>
           </div>
           <div class="card-body">
           <div class="image">
-            <img src="<?php echo 'public/img/'.$news['image']; ?>"> 
+            <img src="<?php echo 'public/img/'.$post[$num]['image']; ?>"> 
           </div>
             <ul class="list-unstyled mt-3 mb-4">
               <li>20 users included</li>
@@ -18,11 +29,11 @@ for ($i=0; $i < 8/3; $i++) { ?>
               <li>Priority email support</li>
               <li>Help center access</li>
             </ul>
-            <div class="batton" data-hren="<?php echo $news["id"]; ?>" id='<?php echo $news["id"]; ?>' idd='<?php echo $num ?>'></div>
+            <div class="batton" data-hren="<?php echo $post[$num]['id']; ?>" id='<?php echo $post[$num]["id"]; ?>' idd='<?php echo $num ?>'></div>
           </div>
         </div>
         <?php $num++;
-      if($num==8){break;} } ?>
+  } ?>
   </div>
 <?php } ?>
 
@@ -31,16 +42,22 @@ for ($i=0; $i < 8/3; $i++) { ?>
     <nav aria-label="Page navigation example">
         <ul class="pagination justify-content-center">
             <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
+                <a class="page-link" href="/" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                     <span class="sr-only">Previous</span>
                 </a>
             </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="/">1</a></li>
+            <?php
+            
+            for($i=0;$i < nums()/6-1;$i++){ ?>
+            
+            <li class="page-item"><a class="page-link" href="?page=<?php echo $i+2 ?>"><?php echo $i+2 ?></a></li>
+            <?php }
+            ?>
+
             <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
+                <a class="page-link" href="?page=<?php echo ceil(nums()/6) ?>" aria-label="Next">
                     <span aria-hidden="true">&raquo;</span>
                     <span class="sr-only">Next</span>
                 </a>
--- db/functions.php
+++ db/functions.php
@@ -1,11 +1,30 @@
 <?php
 
-$link = mysqli_connect('localhost','root','','my_shop_db');
+$link = mysqli_connect('localhost', 'root', '', 'my_shop_db');
 function get_post_by_id($post_id)
 {
     global $link;
-    $sql = "SELECT * FROM items WHERE id = ".$post_id;
+    $sql = "SELECT * FROM items WHERE id = " . $post_id;
     $result = mysqli_query($link, $sql);
     $post = mysqli_fetch_assoc($result);
     return $post;
+}
+
+function get_posts($limit, $offset)
+{
+    global $link;
+    $sql = "SELECT * FROM items LIMIT $limit OFFSET $offset";
+    $result = mysqli_query($link, $sql);
+    $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $posts;
+}
+
+function nums()
+{
+    global $link;
+    $sql = "SELECT count(*) FROM items";
+    $result = mysqli_query($link, $sql);
+    $row = $result->fetch_row();
+    $count = $row[0];
+    return $count;
 }
